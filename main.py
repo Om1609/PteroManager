8da352ad5d65efd726f317f47728ca2c3d424c37
That GenZ Gamer <78960084+ThatGenZGamer48@users.noreply.github.com>
2022-02-20 12:15:55 +0530

    Subclass Bot

--- main.py
+++ main.py
@@ -2,20 +2,14 @@ import discord
 from discord.commands import Option
 from dotenv import load_dotenv
 from utils.logging import log
+from utils.bot import PteroManager
 import os
 
 # Load the env file
 load_dotenv()
 
 # Initiate the PyCord Bot.
-bot = discord.Bot()
-
-
-@bot.event
-async def on_ready():
-    # Log to the user that we're signed in.
-    log(4, f"We have logged in as {bot.user}")
-
+bot = PteroManager()
 
 # Load the cogs
 for filename in os.listdir("./cogs"):
